5634c726933b99bbdedeccacf768bccc1ef668d0
Martin O'Hanlon <martin@ohanlonweb.com>
2017-04-05 08:33:17 +0100

    Update test_blue_dot.py

--- tests/test_blue_dot.py
+++ tests/test_blue_dot.py
@@ -4,5 +4,5 @@ from bluedot.mock import MockBlueDot
 
 def test_client_connected():
     bd = MockBlueDot()
-    bd.mock_client_connected()
+    bd._server.mock_client_connected()
     assert bd.is_connected
